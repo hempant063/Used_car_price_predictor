d36d2282e9044aaa42688ffd60f97b7a146582e0
Hans <hempant063@gmail.com>
2026-09-06 13:25:51 +0530

    created first backend example

--- app/main.py
+++ app/main.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
-import uvicorn
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from fastapi import Form
 
+
+templates=Jinja2Templates(directory='app/templates')
 
 app=FastAPI()
 
 @app.get('/')
-def greet():
-    return {'Hello world'}
+def home(request:Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='index.html',
+        context={
+            'page':'Home'
+        }
+    )
+
+
+@app.post('/predict')
+def prediction(request:Request,company:str=Form(...),age:int=Form(...)):
+    return templates.TemplateResponse(
+        request=request,
+        name='index.html',
+        context={
+            'company':company,
+            'age':age,
+        }
+    )
